1e3cd830acd9510fb511191f3e42626560ae1c4f
Xan López <xan@igalia.com>
2022-01-18 10:34:38 +0100

    i64bigreturn: fix test, cannot use huge immediate numbers without quoting

--- tests/i64bigreturn.js
+++ tests/i64bigreturn.js
@@ -9,7 +9,7 @@ let instance = instantiate(`
 
 for (let i = 0; i < 10; i++) {
     let result = instance.exports.watret();
-    if (!(result == BigInt(9223372036854775807)))
-        throw new Error("Result should be 9223372036854775807, instead was: " + result);
+    if (!(result == BigInt("9223372036854775807")))
+        throw new Error("Result should be " + Number(9223372036854775807).toString(16) +", instead was: " + (Number(result).toString(16)));
 }
 print("Success!");
