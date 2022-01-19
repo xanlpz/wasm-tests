178c7c588001da4d7dd656534bedc6cf2e4fde46
Geza Lore <gezalore@gmail.com>
2022-01-19 16:47:52 +0000

    i64return: test with non-zero in both halves of returned value

--- tests/i64return.js
+++ tests/i64return.js
@@ -3,14 +3,15 @@ import { instantiate } from "../lib/wabt-wrapper.js";
 let instance = instantiate(`
 (module
  (func $ret (result i64)
-       i64.const 255)
+       i64.const 0x00_22_44_66_00_aa_cc_ee)
  (export "watret" (func $ret)))
 `);
 
+const expect = BigInt("0x0022446600aaccee")
 for (let i = 0; i < 10; i++) {
-let result = instance.exports.watret();
-if (!Object.is(result, BigInt(255)))
-    throw new Error("Result should be 255, instead was: " + result);
+  const result = instance.exports.watret();
+  if (!Object.is(result, expect))
+      throw new Error("Result should be 0x" + expect.toString(16) + ", instead was: 0x" + result.toString(16));
 }
 print("Success!");
 
