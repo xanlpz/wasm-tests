869bfb7b96b690679b6f1a28e28d8c23158df2a6
Juliana Sims <jsims@igalia.com>
2022-09-21 09:15:16 -0500

    Add i64stackargs.js, increase arg count in i32stackargs.js

--- tests/i64stackargs.js
+++ tests/i64stackargs.js
@@ -2,7 +2,7 @@ import { instantiate } from "../lib/wabt-wrapper.js";
 
 let instance = instantiate(`
 (module
- (func $stackargs (param i32) (param i32) (param i32) (param i32) (param i32) (param i32) (param i32) (param i32) (param i32) (result i32)
+ (func $stackargs (param i64) (param i64) (param i64) (param i64) (param i64) (param i64) (param i64) (param i64) (param i64) (result i64)
        local.get 0
        local.get 1
        local.get 2
@@ -12,20 +12,20 @@ let instance = instantiate(`
        local.get 6
        local.get 7
        local.get 8
-       i32.add
-       i32.add
-       i32.add
-       i32.add
-       i32.add
-       i32.add
-       i32.add
-       i32.add)
+       i64.add
+       i64.add
+       i64.add
+       i64.add
+       i64.add
+       i64.add
+       i64.add
+       i64.add)
  (export "watstackargs" (func $stackargs)))
 `);
 
 for (let i = 0; i < 10; i++) {
-    let result = instance.exports.watstackargs(1, 2, 3, 4, 5, 6, 7, 8, 9);
-    if (!Object.is(result, 45))
+    let result = instance.exports.watstackargs(1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n);
+    if (!Object.is(result, 45n))
         throw new Error("Result should be 45, instead was " + result);
     else
         print("Success!");
